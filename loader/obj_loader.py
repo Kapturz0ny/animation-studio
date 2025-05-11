91eb4671ecc7691ecd084264ad31ab065d108877
Julia Czyzewska <julaczyzewska28@gmail.com>
2025-05-11 12:20:04 +0200

    wiecej przykladow .obj plikow

--- loader/obj_loader.py
+++ loader/obj_loader.py
@@ -14,7 +14,7 @@ def load_obj(filename):
                         print(f"Skipping malformed vertex line: {line.strip()}")
                 elif line.startswith('f '):
                     parts = line.strip().split()
-                    # Handle different number of vertices per face.
+                    # Handle different number of vertices per face
                     face = []
                     for p in parts[1:]:
                         try:
@@ -34,31 +34,3 @@ def load_obj(filename):
         print(f"An error occurred while loading the file: {e}")
         return [], []
     return vertices, faces
-
-
-# def main():
-
-#     filename = "obj_file_expl/FinalBaseMesh.obj"  # Changed to a raw string
-#     vertices, faces = load_obj(filename)
-
-#     if vertices and faces: # Check if load_obj was successful
-#         print(f"Loaded {filename} successfully.")
-#         print(f"Number of vertices: {len(vertices)}")
-#         print(f"Number of faces: {len(faces)}")
-
-#         # Print the first 5 vertices
-#         print("\nFirst 5 vertices:")
-#         for i, vertex in enumerate(vertices[:5]):
-#             print(f"  {i+1}: {vertex}")
-
-#         # Print the first 5 faces
-#         print("\nFirst 5 faces:")
-#         for i, face in enumerate(faces[:5]):
-#             #  Adjust the indices for display (add 1)
-#             display_face = [index + 1 for index in face]
-#             print(f"  {i+1}: {display_face}")
-#     else:
-#         print(f"Failed to load {filename}.")
-
-# if __name__ == "__main__":
-#     main()
